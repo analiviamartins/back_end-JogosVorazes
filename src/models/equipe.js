9d33b11949b64c58be912240b04af2107839b287
Richard Macedo <richardmacedo.564@gmail.com>
2023-11-21 10:53:36 -0300

    equipe terminada

---
+++ src/models/equipe.js
@@ -0,0 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
+export class Integrantes {
+    constructor(nome, idade, email, hobby, img) {
+        this.id = uuidv4();
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+        this.hobby = hobby;
+        this.img = img;
+    }
+}
